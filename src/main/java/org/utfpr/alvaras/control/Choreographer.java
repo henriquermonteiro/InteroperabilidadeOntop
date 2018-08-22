f72acb10782fdc0a0979791c801b8e669b1041a4
henriquermonteiro <henrique.r.monteiro@hotmail.com>
2018-08-22 06:50:36 -0300

    Update Choreographer.java

--- src/main/java/org/utfpr/alvaras/control/Choreographer.java
+++ src/main/java/org/utfpr/alvaras/control/Choreographer.java
@@ -10,5 +10,20 @@ package org.utfpr.alvaras.control;
  * @author henrique
  */
 public class Choreographer {
+    public static ResponsibilityChain<Alvara> getDictionaryChain(){
+        ToUpperChainLink toUpp = new ToUpperChainLink();
+        ReplacerChainLink replacer = new ReplacerChainLink();
+        TrimChainLink trimer = new TrimChainLink();
+        DictionaryBuilderLink dic = new DictionaryBuilderLink();
+        
+        trimer.addLink(dic);
+        replacer.addLink(trimer);
+        toUpp.addLink(replacer);
+        
+        return return toUpp;
+    }
     
+    public static ResponsibilityChain<Alvara> getExecutionChain(){
+        return null;
+    }
 }
